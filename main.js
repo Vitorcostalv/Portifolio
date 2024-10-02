8d18f3774f7505104ea5a78f40baf6069576b37c
Vitorcostalv <vitorcosta99900@gamil.com>
2024-10-02 10:36:53 -0300

    JavaScript para animações ou funcionalidades extras que pretendo implementar

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// JavaScript para animações ou funcionalidades extras
+// JavaScript para animações ou funcionalidades extras que pretendo implementar
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Portfólio carregado com sucesso!");
 });
